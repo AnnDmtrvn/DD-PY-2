41da5199f8fb145e845624140959d17ff8cda082
AnnDmtrvn <115590090+AnnDmtrvn@users.noreply.github.com>
2022-12-11 23:55:19 +0300

    lab1task1

---
+++ lab1.py
@@ -0,0 +1,105 @@
+
+import doctest
+
+class Cake:
+    def __init__(self, weight, shape):
+        self.colour = weight
+        self.shape = shape
+        """
+        Создание и подготовка к работе объекта "Торт"
+        :param weight: Вес торта
+        :param shape: Форма торта
+        Примеры:
+        >>> cake = Cake(1, round)  # инициализация экземпляра класса
+        """
+
+    def cut_cake(self):
+        """
+        Функция которая проверяет можно ли разрезать торт
+        :return: да или нет
+        Примеры:
+        >>> cake = cake(1, round)
+        >>> cake.is_cut()
+        """
+
+    def eat_cake(self):
+        """
+        Функция которая проверяет можно ли съесть торт
+        :return: да или нет
+        Примеры:
+        >>> cake = cake(1, round)
+        >>> cake.is_eat()
+        """
+        ...
+
+if __name__ == "__main__":
+    doctest.testmod()  # тестирование примеров, которые находятся в документации
+
+class Cat:
+    def __init__(self, breed: str, name: str):
+        self.breed = breed
+        self.name = name
+        """
+        Создание и подготовка к работе объекта "Кот"
+        :param breed: Порода кота
+        :param name: Кличка кота
+        Примеры:
+        >>> cat = Cat("british", "Tom")  # инициализация экземпляра класса
+        """
+
+    def pet_cat(self):
+        """
+        Функция которая проверяет можно ли разрезать торт
+        :return: да или нет
+        Примеры:
+        >>> cake = cake(1, round)
+        >>> cake.is_cut()
+        """
+
+    def def play_cat(self):
+        """
+        Функция которая проверяет можно ли съесть торт
+        :return: да или нет
+        Примеры:
+        >>> cake = cake(1, round)
+        >>> cake.is_eat()
+        """
+        ...
+
+if __name__ == "__main__":
+    doctest.testmod()  # тестирование примеров, которые находятся в документации
+
+
+class IceCream
+    def __init__(self, taste, sort):
+        self.taste = taste
+        self.sort = sort
+        """
+        Создание и подготовка к работе объекта "Мороженное"
+        :param breed: Порода кота
+        :param name: Кличка кота
+        Примеры:
+        >>> cat = Cat("british", "Tom")  # инициализация экземпляра класса
+        """
+
+    def sell_icecream(self):
+        """
+        Функция которая проверяет можно ли разрезать торт
+        :return: да или нет
+        Примеры:
+        >>> cake = cake(1, round)
+        >>> cake.is_cut()
+        """
+
+    def buy_icecream(self):
+        """
+        Функция которая проверяет можно ли съесть торт
+        :return: да или нет
+        Примеры:
+        >>> cake = cake(1, round)
+        >>> cake.is_eat()
+        """
+        ...
+
+if __name__ == "__main__":
+    doctest.testmod()  # тестирование примеров, которые находятся в документации
